c61d4d5babb6f55f57460d57f60bb9054d86a2aa
Bryan Gomes <bryanpgomes@gmail.com>
2022-12-15 13:41:53 -0500

    Lecture notes

---
+++ express_server.js
@@ -0,0 +1,52 @@
+const express = require('./node_modules/express');
+const app = express();
+const bodyParser = require("body-parser");
+//const cookieParser = require('cookie-parser');
+const cookieSession = require('cookie-session');
+const saltRounds = 10;
+//Apps for template, injecting text info into the code, and one for cookie encrption
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+//app.use(cookieParser());
+app.use(cookieSession({
+  name: 'session',
+  keys: ['ddb870d3e75eb646cad9ae1f1c5167a2', '6d576e63daf9125457889ce9049c5725']
+}));
+app.use(express.static(__dirname + '/views'));
+//Port
+const PORT = process.env.PORT || 8080;
+
+
+
+
+//---------------LISTEN APPS------------------------------
+ 
+// Trigger a listen action, on a specific port (8080) and do a callback if it worked
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
+//---------------GET APPS------------------------------
+
+
+// If we have a GET request, asking for /urls.json, we do the callback
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/", (req, res) => {
+  res.render("exampleOne");
+});
+
+app.get("/exampleTwo", (req, res) => {
+  res.render("exampleTwo");
+});
+
+app.get("/exampleThree", (req, res) => {
+  res.render("exampleThree");
+});
+
+//Indexof all short URLs, now with a filtered function
+app.get("/urls", (req, res) => {
+  res.render("urls_index");
+});
